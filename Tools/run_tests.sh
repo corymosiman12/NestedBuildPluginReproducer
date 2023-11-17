0304f9adeaf558d63ba4709b1ce7b36bd1f8f38f
Cory Mosiman <cory.mosiman12@gmail.com>
2023-11-17 16:33:20 -0500

    works

--- Tools/run_tests.sh
+++ Tools/run_tests.sh
@@ -4,18 +4,25 @@ set -euo pipefail
 
 run() {
     TOOLCHAIN=com.apple.dt.toolchain.XcodeDefault
+    IOS_VERSION=${IOS_VERSION:-17.0.1}
+    SDK=${SDK:-iphonesimulator17.2}
+    BUILD_DIR="$(PWD)/.xcbuild"
+
     SCHEME=FooPackageTests
+    PROJECT=App/ABigFoo.xcodeproj
     BUNDLE_PATH="$(PWD)/$SCHEME.xcresult"
+    DESTINATION="platform=iOS Simulator,OS=$IOS_VERSION,name=iPhone 12 Pro"
+    rm -rf "$BUILD_DIR"
     rm -rf "$BUNDLE_PATH"
     xcrun --toolchain "$TOOLCHAIN" \
             xcodebuild test \
-            -project "App/ABigFoo.xcodeproj" \
+            -project "$PROJECT" \
             -toolchain "$TOOLCHAIN" \
             -configuration Debug \
-            -destination 'platform=iOS Simulator,name=iPhone 14 Pro,OS=17.0.1'  \
-            -sdk iphonesimulator17.0 \
+            -destination "$DESTINATION"  \
             -scheme "$SCHEME" \
             -enableCodeCoverage YES \
+            -derivedDataPath "$BUILD_DIR" \
             -skipPackagePluginValidation \
             -disableAutomaticPackageResolution \
             -quiet \
